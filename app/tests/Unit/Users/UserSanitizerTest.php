c8de07340a43213cd00f4e2d4ec985d76b48f2c5
Sabri Taieb <sabritaiebdev@gmail.com>
2023-12-05 05:10:53 +0100

    Refactor unit UserSanitizerTest class

--- app/tests/Unit/Users/UserSanitizerTest.php
+++ app/tests/Unit/Users/UserSanitizerTest.php
@@ -3,93 +3,66 @@
 namespace Tests\Unit\Users;
 
 use App\Users\UserSanitizer;
-use App\Users\UserType;
 use PHPUnit\Framework\TestCase;
+use Tests\_data\UserData;
 
 class UserSanitizerTest extends TestCase
 {
-    public function testSanitizesFirstNameSuccessfully(): void
+    /**
+     * @dataProvider userDataProvider
+     * @param $key
+     * @param $value
+     * @param $expectedValue
+     * @return void
+     */
+    public function testSanitizesDifferentValues($key, $value, $expectedValue): void
     {
-        $userData = $this->userData();
-        $userData['first_name'] = " $*J'0hn_ -doe ";
+        $userData = UserData::memberOne();
+        $userData[$key] = $value;
         $userSanitizer = new UserSanitizer();
         $sanitizedData = $userSanitizer->sanitize($userData);
 
-        $this->assertSame("J'hn doe", $sanitizedData['first_name']);
+        $this->assertSame($expectedValue, $sanitizedData[$key]);
     }
 
-    public function testSanitizesFirstNameFromXssAttacks(): void
+    public static function userDataProvider(): array
     {
-        $userData = $this->userData();
-        $userData['first_name'] = "<script>alert('XSS');</script>";
-        $userSanitizer = new UserSanitizer();
-        $sanitizedData = $userSanitizer->sanitize($userData);
-
-        $this->assertSame("scriptalert'XSS'script", $sanitizedData['first_name']);
-    }
-
-    public function testSanitizesLastNameSuccessfully(): void
-    {
-        $userData = $this->userData();
-        $userData['last_name'] = " @*$&^11 -b'en";
-        $userSanitizer = new UserSanitizer();
-        $sanitizedData = $userSanitizer->sanitize($userData);
-
-        $this->assertSame("b'en", $sanitizedData['last_name']);
-    }
-
-    public function testSanitizesLastNameFromXssAttacks(): void
-    {
-        $userData = $this->userData();
-        $userData['last_name'] = 'Mocha##<IMG SRC="mocha:[code]">##1';
-        $userSanitizer = new UserSanitizer();
-        $sanitizedData = $userSanitizer->sanitize($userData);
-
-        $this->assertSame('MochaIMG SRCmochacode', $sanitizedData['last_name']);
-    }
-
-    public function testSanitizesEmailFromXssAttacks(): void
-    {
-        $userData = $this->userData();
-        $userData['email'] = '“><svg/onload=confirm(1)>”@gmail.com';
-        $userSanitizer = new UserSanitizer();
-        $sanitizedData = $userSanitizer->sanitize($userData);
-
-        $this->assertSame('svgonload=confirm1@gmail.com', $sanitizedData['email']);
-    }
-
-    public function testSanitizesCreatedAt(): void
-    {
-        $userData = $this->userData();
-        $userData['created_at'] = '10';
-        $userSanitizer = new UserSanitizer();
-        $sanitizedData = $userSanitizer->sanitize($userData);
-
-        $this->assertSame(10, $sanitizedData['created_at']);
-    }
-
-    public function testSanitizesUpdateAt(): void
-    {
-        $userData = $this->userData();
-        $userData['updated_at'] = '999';
-        $userSanitizer = new UserSanitizer();
-        $sanitizedData = $userSanitizer->sanitize($userData);
-
-        $this->assertSame(999, $sanitizedData['updated_at']);
-    }
-
-    private function userData(): array
-    {
-        $now = time();
-
         return [
-            'email' => 'test@test.com',
-            'first_name' => 'John',
-            'last_name' => 'Doe',
-            'password' => '12345678',
-            'type' => UserType::Member->value,
-            'created_at' => $now,
-            'updated_at' => $now,
+            'Sanitizes first_name' => [
+                'key' => 'first_name',
+                'value' => ' $*J\'0hn_ -doe ',
+                'expectedValue' => "J'hn doe"
+            ],
+            'Sanitizes first_name from XSS' => [
+                'key' => 'first_name',
+                'value' => "<script>alert('XSS');</script>",
+                'expectedValue' => "scriptalert'XSS'script"
+            ],
+            'Sanitizes last_name' => [
+                'key' => 'last_name',
+                'value' => ' @*$&^11 -b\'en',
+                'expectedValue' => "b'en"
+            ],
+            'Sanitizes last_name from XSS' => [
+                'key' => 'last_name',
+                'value' => 'Mocha##<IMG SRC="mocha:[code]">##1',
+                'expectedValue' => "MochaIMG SRCmochacode"
+            ],
+            'Sanitizes email' => [
+                'key' => 'email',
+                'value' => '“><svg/onload=confirm(1)>”@gmail.com',
+                'expectedValue' => 'svgonload=confirm1@gmail.com'
+            ],
+            'Sanitizes created_at' => [
+                'key' => 'created_at',
+                'value' => '10',
+                'expectedValue' => 10
+            ],
+            'Sanitizes updated_at' => [
+                'key' => 'updated_at',
+                'value' => '999',
+                'expectedValue' => 999
+            ],
         ];
     }
 }
