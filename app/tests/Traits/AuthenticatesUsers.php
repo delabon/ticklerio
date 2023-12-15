b4e332fe3b552df97098842f2dca31e950c138fd
Sabri Taieb <sabritaiebdev@gmail.com>
2023-12-15 10:10:15 +0100

    Refactor AuthenticatesUsers trait

--- app/tests/Traits/AuthenticatesUsers.php
+++ app/tests/Traits/AuthenticatesUsers.php
@@ -2,9 +2,7 @@
 
 namespace Tests\Traits;
 
-use App\Users\User;
 use App\Users\UserRepository;
-use App\Users\UserType;
 use Tests\_data\UserData;
 
 trait AuthenticatesUsers
@@ -16,7 +14,6 @@ trait AuthenticatesUsers
     {
         $user = UserRepository::make(UserData::memberOne());
         $user->setId(1);
-        $user->setType(UserType::Member->value);
         $this->auth->login($user);
     }
 
@@ -24,7 +21,6 @@ trait AuthenticatesUsers
     {
         $admin = UserRepository::make(UserData::adminData());
         $admin->setId(2);
-        $admin->setType(UserType::Admin->value);
         $this->auth->login($admin);
     }
 }
