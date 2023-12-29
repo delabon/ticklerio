a63153c4681846504e1a7c32be73830cdee06af6
Sabri Taieb <sabritaiebdev@gmail.com>
2023-12-29 09:26:08 +0100

    Add unit test for \App\Core\DatabaseOperationFileHandler::validateFileNames

--- app/tests/Unit/Core/DatabaseOperationFileHandlerTest.php
+++ app/tests/Unit/Core/DatabaseOperationFileHandlerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Core;
 
 use App\Core\DatabaseOperationFileHandler;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class DatabaseOperationFileHandlerTest extends TestCase
 {
@@ -17,5 +18,36 @@ class DatabaseOperationFileHandlerTest extends TestCase
         $this->assertCount(2, $result);
         $this->assertContains('1_create_dummy_table.php', $result);
         $this->assertContains('20_create_dummy_table_twenty.php', $result);
+    }
+
+    public function testValidateFileNamesMethodValidatesFileNamesCorrectly(): void
+    {
+        $fileHandler = new DatabaseOperationFileHandler('migration');
+
+        $fileHandler->validateFileNames([
+            '1_create_dummy_table.php',
+            '20_create_dummy_table_twenty.php'
+        ]);
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testValidateFileNamesMethodThrowsExceptionWhenFileNamesAreIncorrect(): void
+    {
+        $filePath = 'create_dummy_table.php';
+        $fileHandler = new DatabaseOperationFileHandler('migration');
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage(
+            sprintf(
+                "The %s file name '%s' is invalid. It should be in the format of '[1-9]_file_name.php'.",
+                'migration',
+                $filePath
+            )
+        );
+
+        $fileHandler->validateFileNames([
+            $filePath,
+        ]);
     }
 }
