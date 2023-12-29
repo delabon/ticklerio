5d0def7b4b6f8bd9a6c76680a14cefb120ec7fe1
Sabri Taieb <sabritaiebdev@gmail.com>
2023-12-29 14:18:55 +0100

    Add CLI scripts

---
+++ app/database/rollback-seeds.php
@@ -0,0 +1,18 @@
+#!/usr/local/bin/php
+<?php
+
+/**
+ * Runs all seeder scripts
+ */
+
+use App\Core\Seeding\Seeder;
+use App\Core\Utilities\ClassNameConverter;
+
+$container = require_once __DIR__ . '/../src/bootstrap.php';
+
+$migration = new Seeder(
+    $container->get(PDO::class),
+    new ClassNameConverter(),
+    __DIR__ . '/migrations/'
+);
+$migration->rollback();
