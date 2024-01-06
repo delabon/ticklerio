fd5fea9fee3f6ddfbb97e5f8f531c757c9c1b3db
Sabri Taieb <sabritaiebdev@gmail.com>
2024-01-06 05:59:55 +0100

    Update show view for ticket

--- app/resources/views/tickets/show.php
+++ app/resources/views/tickets/show.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Tickets\Ticket;
+use App\Tickets\TicketStatus;
 
 require __DIR__ . '/../parts/header.php';
 
@@ -12,9 +13,16 @@ require __DIR__ . '/../parts/header.php';
             <div class="col-6">
                 <h1 class="h3"><?= $ticket->getTitle() ?></h1>
             </div>
+
             <div class="col-6 text-end">
-                <a href="/tickets/edit/<?= $ticket->getId() ?>" class="btn btn-primary">Edit</a>
-                <button id="delete-ticket-btn" class="btn btn-danger">Delete</button>
+                <?php if ($ticket->getUserId() === currentUserId() || isAdmin()) : ?>
+                    <a href="/tickets/edit/<?= $ticket->getId() ?>" class="btn btn-primary">Edit</a>
+                    <button id="delete-ticket-btn" class="btn btn-danger">Delete</button>
+                <?php endif ?>
+
+                <?php if ($ticket->getStatus() === TicketStatus::Publish->value) : ?>
+                    <a href="#reply-form" class="btn btn-secondary">Reply</a>
+                <?php endif ?>
             </div>
         </div>
 
